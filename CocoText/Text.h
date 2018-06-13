be73a4d1b7c27e3c319c31ffa860eeb04ac19e97
ZR <zhangru2@kanfanews.com>
2018-06-13 15:18:10 +0800

    firt commit

---
+++ CocoText/Text.h
@@ -0,0 +1,13 @@
+//
+//  Text.h
+//  CocoText
+//
+//  Created by zr on 2018/6/13.
+//  Copyright © 2018年 ZR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Text : NSObject
+
+@end
---
+++ CocoText/Text.m
@@ -0,0 +1,13 @@
+//
+//  Text.m
+//  CocoText
+//
+//  Created by zr on 2018/6/13.
+//  Copyright © 2018年 ZR. All rights reserved.
+//
+
+#import "Text.h"
+
+@implementation Text
+
+@end
